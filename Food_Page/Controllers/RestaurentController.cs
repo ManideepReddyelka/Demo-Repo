3086c9924f27965cc43ac9555bb1e56e457e2752
Manideep <manideepreddyelka@gmail.com>
2023-12-04 19:26:26 -0500

    Add project files.

---
+++ Food_Page/Controllers/RestaurentController.cs
@@ -0,0 +1,32 @@
+﻿using Food_Page.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Food_Page.Controllers
+{
+    public class RestaurentController : Controller
+    {
+        // GET: Restaurent
+        public ActionResult GetMenuDetails()
+        {
+            MVC_PAGEEntities mVC_PAGEEntities = new MVC_PAGEEntities();
+            List<Restaurent> restaurents =  mVC_PAGEEntities.Restaurents.ToList();
+            return View(restaurents);
+        }
+        public ActionResult GetItemBySno(int Sno)
+        {
+            MVC_PAGEEntities mVC_PAGEEntities = new MVC_PAGEEntities();
+            var restaurent = mVC_PAGEEntities.Restaurents.Find(Sno);
+            return View(restaurent); 
+        }
+        public ActionResult ItemDetails()
+        {
+            return View();
+
+        }
+    }
+}
